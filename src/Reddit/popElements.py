fbc85eb3687467f3872b75735251b8bb1aff4e29
Joey Drew <joey@joeydrew.com>
2017-11-17 14:35:21 -0600

    Added Reddit folder
    Includes scripts (Python 2, sorry) used to pull reddit posts and the
    merged .json files themselves. I excluded the bulk data.

---
+++ src/Reddit/popElements.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+filenameList = []
+
+def popElement(directory, filename, element):
+    with open(directory + '/' + filename, 'r') as data_file:
+        data = json.load(data_file)
+    try:
+        data[1]
+    except IndexError:
+        filenameList.append(filename)
+    # data.pop(element)
+    # with open(directory + '/' + filename, 'w') as data_file:
+    #     data = json.dump(data, data_file)
+    # print filename
+
+for file in os.listdir('leagueoflegendsOct'):
+    popElement('leagueoflegendsOct', file, 1)
+
+print len(filenameList)
